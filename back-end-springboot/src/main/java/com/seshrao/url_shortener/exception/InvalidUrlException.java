3aed13ea20711520188df66da06a99a074aec245
PSesharao <paritalasesharao@gmail.com>
2023-09-04 19:54:46 +0530

    throw exception when it is a bad URL

---
+++ back-end-springboot/src/main/java/com/seshrao/url_shortener/exception/InvalidUrlException.java
@@ -0,0 +1,8 @@
+package com.seshrao.url_shortener.exception;
+
+public class InvalidUrlException extends RuntimeException {
+    public InvalidUrlException(String message) {
+        super(message);
+    }
+}
+
