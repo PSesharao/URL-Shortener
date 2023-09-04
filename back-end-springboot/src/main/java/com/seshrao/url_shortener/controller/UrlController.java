23b273d8cb32635a0789cc2c73659339cf412edc
PSesharao <paritalasesharao@gmail.com>
2023-09-04 17:54:45 +0530

    add controller

---
+++ back-end-springboot/src/main/java/com/seshrao/url_shortener/controller/UrlController.java
@@ -0,0 +1,24 @@
+package com.seshrao.url_shortener.controller;
+
+import com.seshrao.url_shortener.model.Url;
+import com.seshrao.url_shortener.service.UrlService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("url/shortner")
+public class UrlController {
+
+    @Autowired
+    private UrlService urlService;
+
+    @GetMapping("/{id}")
+    public String getOriginlUrl(@PathVariable String id) {
+        return urlService.getOriginlUrl(id);
+    }
+
+    @PostMapping
+    public Url generateShortUrl(@RequestBody String url) {
+        return urlService.generateShortUrl(url);
+    }
+}
