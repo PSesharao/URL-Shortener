6a62825e319bd1dc2ce4d05cba50579ec0e7e093
PSesharao <paritalasesharao@gmail.com>
2023-09-04 17:58:44 +0530

    add entity

---
+++ back-end-springboot/src/main/java/com/seshrao/url_shortener/model/Url.java
@@ -0,0 +1,23 @@
+package com.seshrao.url_shortener.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Url {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String shorturl;
+    private String originalurl;
+
+}
