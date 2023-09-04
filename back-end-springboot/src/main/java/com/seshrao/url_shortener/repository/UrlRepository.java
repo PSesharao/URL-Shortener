fa7f68d30159d826b4559e9ea7d4fcad3e2aed2d
PSesharao <paritalasesharao@gmail.com>
2023-09-04 18:00:11 +0530

    add repository

---
+++ back-end-springboot/src/main/java/com/seshrao/url_shortener/repository/UrlRepository.java
@@ -0,0 +1,12 @@
+package com.seshrao.url_shortener.repository;
+
+
+import com.seshrao.url_shortener.model.Url;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface UrlRepository  extends JpaRepository<Url,Integer> {
+
+    @Query(value = "select originalurl from Url where shorturl= ?1" , nativeQuery = true)
+    String findByShortUrl(String id);
+}
