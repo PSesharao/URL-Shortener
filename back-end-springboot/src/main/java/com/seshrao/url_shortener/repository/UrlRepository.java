775e4faf84541f2f5391e331900165f333fb2c79
PSesharao <paritalasesharao@gmail.com>
2023-09-04 19:45:11 +0530

    retrieve original url from repo

--- back-end-springboot/src/main/java/com/seshrao/url_shortener/repository/UrlRepository.java
+++ back-end-springboot/src/main/java/com/seshrao/url_shortener/repository/UrlRepository.java
@@ -9,4 +9,6 @@ public interface UrlRepository  extends JpaRepository<Url,Integer> {
 
     @Query(value = "select originalurl from Url where shorturl= ?1" , nativeQuery = true)
     String findByShortUrl(String id);
+
+    Url findByOriginalurl(String url);
 }
