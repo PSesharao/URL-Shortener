2ce487e9ca8005c8d27be5389b442b41689688d0
PSesharao <paritalasesharao@gmail.com>
2023-09-04 19:47:57 +0530

    check for provided long URL already exists in the database

--- back-end-springboot/src/main/java/com/seshrao/url_shortener/service/UrlService.java
+++ back-end-springboot/src/main/java/com/seshrao/url_shortener/service/UrlService.java
@@ -1,6 +1,7 @@
 package com.seshrao.url_shortener.service;
 
 
+import com.seshrao.url_shortener.exception.InvalidUrlException;
 import com.seshrao.url_shortener.model.Url;
 import com.seshrao.url_shortener.repository.UrlRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,7 +23,13 @@ public class UrlService {
     public Url generateShortUrl(String url) {
         if(! isUrlValid(url)) {
             System.out.println("URL is not valid");
-            return null;
+            throw new InvalidUrlException("URL is not valid: " + url);
+            // return null;
+        }
+
+        Url existingUrl = urlRepository.findByOriginalurl(url);
+        if (existingUrl != null) {
+            return existingUrl; // Return the existing short URL if the long URL already exists
         }
 
         Url urlObject = new Url();
